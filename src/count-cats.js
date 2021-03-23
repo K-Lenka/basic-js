90b839c323addc2f6b949f1d02d1d317283da3a2
Elena <alena1027@gmail.com>
2021-03-23 14:33:30 -0300

    feat:tast solve

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+// [[1, 2, '^^'], ['sdfg', 0, 1]]
+module.exports = function countCats(matrix) {
+    return matrix.flat(Infinity).filter(el => el === '^^').length
+
+
+    // let count = 0;
+    // for (let i = 0; i < matrix.length; i++) {
+    //     for (let j = 0; j < matrix[i].length; j++) {
+    //         if (matrix[i][j] === '^^')
+    //             count += 1;
+    //     }
+    // }
+    // return count;
 };
